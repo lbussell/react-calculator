fb0c17dc2df09f30f61ed820e77f8a43e90da437
Logan Bussell <lsbussell@gmail.com>
2019-05-30 17:35:26 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "lbussell.github.io/calculator/precache-manifest.d35540c887a57ccca899c680bf9eb03e.js"
+  "lbussell.github.io/react-calculator/precache-manifest.5d91f0e810edff155ef31fb74441c2b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("lbussell.github.io/calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("lbussell.github.io/react-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
